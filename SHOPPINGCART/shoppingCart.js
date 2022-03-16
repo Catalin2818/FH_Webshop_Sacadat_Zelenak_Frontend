05066aae7257c4e033cebfbf440b108904c297cd
Catalin <sacadatcatalin@gmail.com>
2022-03-16 15:50:50 +0100

    new product cart tryout

--- SHOPPINGCART/shoppingCart.js
+++ SHOPPINGCART/shoppingCart.js
@@ -38,26 +38,11 @@ function addProduct(){
     var addProductIdInput = document.getElementById("addProductId").value;
     var addFinishedInput = document.getElementById("addFinished").value;
 
-
-    /*var addProduktNameInput = document.getElementById("addProductName").value;
-    var addProduktHerkunftInput = document.getElementById("addProductOrigin").value;
-    var addProduktBeschrInput = document.getElementById("addProductDesc").value;
-    var addProduktAllergeneInput = document.getElementById("addProductAllergens").value;
-    var addProduktPreisInput = document.getElementById("addProductPrice").value;
-    var addProduktStueckanzahlInput = document.getElementById("addProductQuantity").value;
-    var addProduktKategorieInput = document.getElementById("addProductCategory").value;
-    var addProduktAgbeholtInput = document.getElementById("addProductPickup").value;*/
-
     var shoppingCart = '{'
         //+'"id": 1,'
         +'"user":"' +addUserIdInput+'",'
         +'"products":"' +addProductIdInput+'",'
         +'"finished":"' +addFinishedInput+'",'
-        /*+'"productAllergens": "'+addProduktAllergeneInput+'",'
-        +'"productPrice": '+addProduktPreisInput+','
-        +'"productQuantity": '+addProduktStueckanzahlInput+','
-        +'"productCategory": "'+addProduktKategorieInput+'",'
-        +'"productPickup": "'+addProduktAgbeholtInput +'"'*/
         +'}';
 
     $.ajax({
@@ -97,7 +82,6 @@ function getAllProducts(){
             console.log(data);
         },
         failure: function(errMsg){alert(errMsg);}
-
     })
 }
 
@@ -107,53 +91,26 @@ function editProductsInModal(event){
     var editShoppingCartUserIdInput = document.getElementById("editUserId");
     var editShoppingCartProductIdInput = document.getElementById("editProductId");
     var editFinishedInput = document.getElementById("editFinished");
-    /*var editProductAllergensInput = document.getElementById("editProductAllergens");
-    var editProductPriceInput = document.getElementById("editProductPrice");
-    var editProductQuantityInput = document.getElementById("editProductQuantity");
-    var editProductCategoryInput = document.getElementById("editProductCategory");
-    var editProductPickupInput = document.getElementById("editProductPickup");*/
 
     editShoppingCartIdInput.innerText = event.parentNode.parentNode.cells[1].innerHTML;
     editShoppingCartUserIdInput.value = event.parentNode.parentNode.cells[2].innerHTML;
     editShoppingCartProductIdInput.value = event.parentNode.parentNode.cells[3].innerHTML;
     editFinishedInput.value = event.parentNode.parentNode.cells[4].innerHTML;
-    /*editProductAllergensInput.value = event.parentNode.parentNode.cells[5].innerHTML;
-    editProductPriceInput.value = event.parentNode.parentNode.cells[6].innerHTML;
-    editProductQuantityInput.value = event.parentNode.parentNode.cells[7].innerHTML;
-    editProductCategoryInput.value = event.parentNode.parentNode.cells[8].innerHTML;
-    editProductPickupInput.value = event.parentNode.parentNode.cells[9].innerHTML;*/
 
 }
 
 function editProduct(){
-
 
     var editShoppingCartIdInput = document.getElementById("editShoppingCartId").innerHTML;
     var editShoppingCartUserIdInput = document.getElementById("editUserId").innerHTML;
     var editShoppingCartProductIdInput = document.getElementById("editProductId").innerHTML;
     var editFinishedInput = document.getElementById("editFinished").value;
 
-    /*var editProductIdInput = document.getElementById("editProductID").innerHTML;
-    var editProductNameInput = document.getElementById("editProductName").value;
-    var editProductOriginInput = document.getElementById("editProductOrigin").value;
-    var editProductDescInput = document.getElementById("editProductDesc").value;
-    var editProductAllergensInput = document.getElementById("editProductAllergens").value;
-    var editProductPriceInput = document.getElementById("editProductPrice").value;
-    var editProductQuantityInput = document.getElementById("editProductQuantity").value;
-    var editProductCategoryInput = document.getElementById("editProductCategory").value;
-    var editProductPickupInput = document.getElementById("editProductPickup").value;*/
-
-
     var shoppingCartT ='{'
         +'"id":' +editShoppingCartIdInput +','
         +'"user":"' +editShoppingCartUserIdInput+'",'
         +'"product":"' +editShoppingCartProductIdInput+'",'
         +'"finished":"' +editFinishedInput+'",'
-        /*+'"productAllergens": "'+editProductAllergensInput+'",'
-        +'"productPrice": '+editProductPriceInput+','
-        +'"productQuantity": '+editProductQuantityInput+','
-        +'"productCategory": "'+editProductCategoryInput+'",'
-        +'"productPickup": "'+editProductPickupInput +'"'*/
         +'}';
 
     $.ajax({
@@ -181,7 +138,6 @@ function editProduct(){
 }
 
 
-
 function deleteProduct(event){
 
     var id = event.parentNode.parentNode.cells[1].innerHTML;
@@ -196,7 +152,6 @@ function deleteProduct(event){
         },
         failure: function(errMsg){alert(errMsg);}
     });
-
 }
 
 function addProductInTable(shoppingCartData){
